8a525bf8e7beb1d52d23e3240c9680ee125cce6c
22050039-Yinhua Chen <12132329@mail.sustech.edu.cn>
2023-02-03 10:56:07 +0800

    pass hello demo

---
+++ src/tb.cc
@@ -0,0 +1,40 @@
+#include "tb.h"
+
+// 1. reset inp   2. generate output
+void tb::source() {
+  // Reset
+  inp.write(0);
+  rst.write(1);
+  wait();
+  rst.write(0);
+  wait();
+
+  sc_int<16> tmp;
+
+  // Send stimulus to FIR
+  for(int i = 0; i < 64; i++) {
+    if(i > 23 && i < 29>)      
+      tmp = 256;
+    else 
+      tmp = 0;
+    inp.write(tmp);
+    wait(); // 64个周期
+  }
+}
+
+// 1. read data from FIR
+void tb::sink() {
+  sc_int<16> indata;
+
+  // Read output coming from DUT
+  for (int i = 0; i < 64; i++) {
+    indata = outp.read();
+    wait();
+    cout << i << ":\t" <<
+      indata.to_int() << endl;
+  }
+
+  // End simulation
+  sc_stop(); // 这个函数放在接收数据的地方调用，目的是为了保证该观察到的现象都观察到了
+}
+
---
+++ src/tb.h
@@ -0,0 +1,27 @@
+#include <systemc.h>
+
+SC_MODULE(tb) {
+  sc_in<bool> clk;
+  sc_out<bool> rst;
+  sc_out< sc_int<16> > inp;
+  sc_in< sc_int<16> > outp;
+
+  // 以下这两个函数会被用在构造函数中，也可以叫做Thread
+  void source(); // 1. reset inp   2. generate output
+  void sink(); // 1. read data from FIR
+  // 把Testbench里的线程分成source和sink是非常重要的, 特别是在涉及到生产者-消费者模型的时候, 你会在以后见识到
+
+  FILE *outfp；
+  // added in tutorial 5 for timestamp -- start
+  sc_time start_time[64], end_time[64], clock_period;
+  // added in tutorial 5 for timestamp -- end
+
+  SC_CTOR(tb) {
+    SC_CTHREAD(source, clk.pos());
+    SC_CTHREAD(sink, clk.pos());
+    // 这里没有reset_signal_is函数的调用，原因是测试床不需要reset信号
+    // 没看到上面那个rst信号是output吗？rst信号是由tb产生的！
+  }
+
+};
+
